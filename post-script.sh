d7b87cf05c45a7b71764629fad89825146ef9a55
mohammmad <captain.misugi11@gmail.com>
2015-11-17 23:15:28 +0330

    salam in post-script

--- post-script.sh
+++ post-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo salam!
+echo salam! salam!
 curl -X POST http://localhost:1235/shutdown || true
 sleep 5
 nohup java -jar ./target/deployment-practice-1.0.SNAPSHOT.jar >/dev/null 2>&1 & 
