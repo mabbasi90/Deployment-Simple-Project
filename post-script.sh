228d455ca65a285da7470c7a5832f6d2d92b4537
mohammmad <captain.misugi11@gmail.com>
2015-11-18 00:08:16 +0330

    adding new student+ fixed

--- post-script.sh
+++ post-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo starting!
-curl -X POST http://localhost:1235/shutdown || true
+curl -o /dev/null -X POST http://localhost:1235/shutdown || true
 sleep 5
 nohup java -jar ./target/deployment-practice-1.0.SNAPSHOT.jar >/dev/null 2>&1 & 
 
