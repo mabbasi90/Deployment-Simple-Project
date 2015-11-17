3005b917bea8d77ca62754b0e68c64866dc87ee2
mohammmad <captain.misugi11@gmail.com>
2015-11-18 00:40:40 +0330

    fina post-script

--- post-script.sh
+++ post-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo starting!
+echo starting final?!
 nohup curl -X POST http://localhost:1235/shutdown || true &
 sleep 5
 nohup java -jar ./target/deployment-practice-1.0.SNAPSHOT.jar & 
