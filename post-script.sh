8e26c9d1c9f9893d676ae6504d79f2144260909b
mohammmad <captain.misugi11@gmail.com>
2015-11-18 00:26:20 +0330

    adding sudo

--- post-script.sh
+++ post-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 echo starting!
-sudo curl -X POST http://localhost:1235/shutdown || true
+nohup curl -X POST http://localhost:1235/shutdown || true &
 sleep 5
-sudo nohup java -jar ./target/deployment-practice-1.0.SNAPSHOT.jar & 
+nohup java -jar ./target/deployment-practice-1.0.SNAPSHOT.jar & 
 
