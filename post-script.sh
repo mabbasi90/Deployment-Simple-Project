0fb59b303611b116dcaa16d948cc745b0614026f
mohammmad <captain.misugi11@gmail.com>
2015-11-18 10:20:15 +0330

    a change test!

--- post-script.sh
+++ post-script.sh
@@ -3,5 +3,5 @@
 echo starting final?!
 curl -X POST http://localhost:1235/shutdown || true
 sleep 5
-nohup java -jar ./target/deployment-practice-1.0.SNAPSHOT.jar & 
+nohup java -jar ./target/deployment-practice-1.0.SNAPSHOT.jar >/home/mohammad/myprogram.out 2>&1 & 
 
