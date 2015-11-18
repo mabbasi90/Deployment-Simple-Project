204aeaf33433c287b85e93a4566c6396f3e5d721
mohammmad <captain.misugi11@gmail.com>
2015-11-18 10:38:08 +0330

    without true

--- post-script.sh
+++ post-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo starting final?!
-curl -X POST http://localhost:1235/shutdown || true
+curl -X POST http://localhost:1235/shutdown
 sleep 5
 nohup java -jar ./target/deployment-practice-1.0.SNAPSHOT.jar >my.out 2>&1 & 
 
