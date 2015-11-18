d7ef5cc60a84268fa4928590abc2365219f344c8
mohammmad <captain.misugi11@gmail.com>
2015-11-18 10:17:05 +0330

    a change test

--- post-script.sh
+++ post-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo starting final?!
-nohup curl -X POST http://localhost:1235/shutdown || true &
+curl -X POST http://localhost:1235/shutdown || true
 sleep 5
 nohup java -jar ./target/deployment-practice-1.0.SNAPSHOT.jar & 
 
